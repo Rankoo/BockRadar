8797c16d1638ecbd9c496ba4bcf56f32ce876eb1
rankoo <marlon971224@gmail.com>
2025-08-10 13:21:48 -0500

    DTO de libros y  modelo de datos para la respuesta de la API

---
+++ Models/OpenLibrarySearchResponse.cs
@@ -0,0 +1,31 @@
+using System.Text.Json.Serialization;
+
+namespace BookRadar.Models
+{
+    public class OpenLibrarySearchResponse
+    {
+        [JsonPropertyName("numFound")]
+        public int NumFound { get; set; }
+
+        [JsonPropertyName("docs")]
+        public List<OpenLibraryDoc> Docs { get; set; } = new();
+    }
+
+    public class OpenLibraryDoc
+    {
+        [JsonPropertyName("title")]
+        public string? Title { get; set; }
+
+        [JsonPropertyName("first_publish_year")]
+        public int? FirstPublishYear { get; set; }
+
+        [JsonPropertyName("publish_year")]
+        public List<int>? PublishYear { get; set; }
+
+        [JsonPropertyName("publisher")]
+        public List<string>? Publisher { get; set; }
+
+        [JsonPropertyName("author_name")]
+        public string? Autor { get; set; }
+    }
+}
